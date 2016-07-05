76d972c6bd0f43451a2acf2f564ab3a0f12c51c1
kaupunki-apina <tomy_salminen@msn.com>
2016-07-05 14:19:46 +0300

    Preliminary work for enemy patrolling.

---
+++ FiveWeeks/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	[SerializeField] private Vision mVision;
+	[SerializeField] private Patrol mPatrol;
+
+	// Current state of awareness.
+	private PatrolState mState {
+		get{
+			return mState;
+		} 
+		set {
+			mState = value;
+			mPatrol.onPatrolStateChanged(mState);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public enum PatrolState {
+		CHASE, PATROL_LOW_ALERT, PATROL_MEDIUM_ALERT, PATROL_HIGH_ALERT, SEARCH
+	};
+
+	public interface Vision {
+		bool canSeePlayer ();
+		void onPlayerSeen ();
+		Vector3 playerLastSeen ();
+	}
+
+	public abstract class Patrol : MonoBehaviour {
+		public abstract void onPatrolStateChanged(PatrolState newState);
+	}
+}
---
+++ FiveWeeks/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class EnemyPatrol : EnemyBehaviour.Patrol {
+
+	[SerializeField] private Transform[] path;
+	private NavMeshAgent agent;
+	private int mCurrentDestination = 0;
+	private float goalRadius = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+
+		agent.autoBraking = false;
+		gotoNextPoint ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (agent.remainingDistance <= goalRadius)
+			gotoNextPoint ();
+	}
+
+	public override void onPatrolStateChanged (EnemyBehaviour.PatrolState newState)
+	{
+		// Do stuff. Like go back to orginal patrol path.
+	}
+
+	protected void gotoNextPoint() {
+		if (path.Length != 0) {
+			mCurrentDestination = (mCurrentDestination + 1) % path.Length;
+			agent.destination = path [mCurrentDestination].position;
+		}
+	}
+}
